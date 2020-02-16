aa02c70114b1fa6d48f504677d72a2ae534c3af7
Arnob Mahmud <60808266+ArnobMahmud@users.noreply.github.com>
2020-02-16 23:25:21 +0600

    Input Dialog Box on Window

---
+++ InputDialog.java
@@ -0,0 +1,17 @@
+/* Author : Arnob Mahmud
+  Mail : arnob.tech.me @ gmail.com
+*/
+
+package swing;
+import javax.swing.JOptionPane;
+
+public class InputDialog  {
+
+	public static void main(String[] args) {
+		
+		String name = JOptionPane.showInputDialog("What is your name ?");
+		JOptionPane.showMessageDialog(null, name + " You are welcome", "Welcome" , JOptionPane.INFORMATION_MESSAGE);	
+
+	}
+
+}
