2c31ba467ab394905bc26ae6bcddca21c56db659
Arnob Mahmud <60808266+ArnobMahmud@users.noreply.github.com>
2020-02-17 00:32:37 +0600

    Confirm Dialog Box on Window

---
+++ ConfirmDialog.java
@@ -0,0 +1,17 @@
+/* 
+   Author : Arnob Mahmud
+   Mail : arnob.tech.me @ gmail.com
+*/
+
+package swing;
+import javax.swing.JOptionPane;
+
+public class ConfirmDialog {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showConfirmDialog(null, "Do you want to quit this program?", "Quit",JOptionPane.YES_NO_OPTION );
+        
+	}
+
+}
