d35fab79684bcb2aac3f0a57a8a7f987d915a66f
Arnob Mahmud <arnob.tech.me@gmail.com>
2020-06-07 23:45:34 +0600

    Update Calculator_Frame.java

--- Calculator_Frame.java
+++ Calculator_Frame.java
@@ -10,8 +10,11 @@ import javax.swing.JFrame;
 public class Calculator_Frame {
 
 	public static void main(String[] args) {
+
 		@SuppressWarnings("unused")
+
 		JFrame frame = new JFrame();
+
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400,500);
