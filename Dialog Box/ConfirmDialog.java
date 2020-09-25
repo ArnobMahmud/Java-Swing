ab3d78e47ea582dfd84d86703080e19823cc5e9d
ArnobMahmud <arnob.tech.me@gmail.com>
2020-09-25 15:16:18 +0600

    files added

--- Dialog Box/ConfirmDialog.java
+++ Dialog Box/ConfirmDialog.java
@@ -4,16 +4,15 @@
    Mail : arnob.tech.me @ gmail.com
 */
 
-package swing;
+package Dialog Box;
 import javax.swing.JOptionPane;
 
 public class ConfirmDialog {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		
 		JOptionPane.showConfirmDialog(null, "Do you want to quit this program?", "Quit",JOptionPane.YES_NO_OPTION );
         
 	}
 
 } 
- 
